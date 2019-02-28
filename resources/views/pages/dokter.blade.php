449d1f6f5d763bfdb7f1e81aa47a9bdb53d17ef2
Fajri <fajrixmail@gmail.com>
2019-02-28 17:51:11 +0700

    Add toggle button

--- resources/views/pages/dokter.blade.php
+++ resources/views/pages/dokter.blade.php
@@ -10,10 +10,10 @@
             <h1 class="text-center">DOKTER</h1>
             <div class="row">
                 <div class="col-lg-3">
-                    <button type="button" class="btn btn-light btn-lg btn-block">
+                    <button type="button" class="btn btn-light btn-lg btn-block" onclick="Dokter1()">
                         Anak <span class="fas fa-angle-right float-right" aria-hidden="true"></span>
                     </button>
-                    <button type="button" class="btn btn-light btn-lg btn-block">
+                    <button type="button" class="btn btn-light btn-lg btn-block" onclick="Dokter2()">
                         Anestasi <span class="fas fa-angle-right float-right" aria-hidden="true"></span>
                     </button>
                     <button type="button" class="btn btn-light btn-lg btn-block">
@@ -32,11 +32,41 @@
                         Saraf <span class="fas fa-angle-right float-right" aria-hidden="true"></span>
                     </button>
                 </div>
-                <div class="col-lg-9">
+                <div id="dokter-1" class="col-lg-9">
                     <div class="row">
                         <div class="col-lg-4">
                             <img src="{{ asset('img/Female doctor.jpg') }}" class="rounded-circle dokter-img" alt="dokter">
                             <p>dr. Tessa Miranda Atmaja, Sp.BP-RE</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="{{ asset('img/Female doctor.jpg') }}" class="rounded-circle dokter-img" alt="dokter">
+                            <p>dr. Lia Damayanti, Sp.PA</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="{{ asset('img/Female doctor.jpg') }}" class="rounded-circle dokter-img" alt="dokter">
+                            <p>dr. Antonius Hocky Pudjiadi, Sp.A(K)</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <img src="{{ asset('img/Female doctor.jpg') }}" class="rounded-circle dokter-img" alt="dokter">
+                            <p>dr. Tessa Miranda Atmaja, Sp.BP-RE</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="{{ asset('img/Female doctor.jpg') }}" class="rounded-circle dokter-img" alt="dokter">
+                            <p>dr. Lia Damayanti, Sp.PA</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="{{ asset('img/Female doctor.jpg') }}" class="rounded-circle dokter-img" alt="dokter">
+                            <p>dr. Antonius Hocky Pudjiadi, Sp.A(K)</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="dokter-2" class="col-lg-9">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <img src="{{ asset('img/Female doctor.jpg') }}" class="rounded-circle dokter-img" alt="dokter">
+                            <p>dr. Sutopo, Sp.BP-RE</p>
                         </div>
                         <div class="col-lg-4">
                             <img src="{{ asset('img/Female doctor.jpg') }}" class="rounded-circle dokter-img" alt="dokter">
