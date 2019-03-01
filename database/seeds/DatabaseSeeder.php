33c866afaffbd06a59508d0951f9441b4b3c2914
ericanthonywu <37053920+ericanthonywu@users.noreply.github.com>
2019-03-01 18:04:19 +0700

    seeding changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker\Factory::create();
+        foreach (range(1,50) as $index){
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt('tes')
+            ]);
+            DB::table('dokter')->insert([
+                'name'=> $faker->name,
+                'tipe_dokter' => $faker->randomDigit,
+                'jadwalawal'=>$faker->time('H:i:s',$max = 'now'),
+                'jadwalakhir'=>$faker->time('H:i:s',$max = 'now'),
+                'keahlian'=>Str::random('10')
+            ]);
+        }
     }
 }
